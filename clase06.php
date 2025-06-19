9471ca6fb4f650c70bbd5ad3965dc3440ae386f0
JdaniloChin <danilochin91@gmail.com>
2025-06-18 19:39:16 -0600

    commit mi primer php

---
+++ clase06.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/style2.css">
+    <title>Mi primer PHP</title>
+</head>
+<body>
+    <?php
+    // Agregar un comentario en PHP 
+    // Manejo de variables 
+    $nombre= "Carlos";
+    $edad = 30;
+    $nota = 85.5;
+    $aprobado= true;
+
+    // Imprimir 
+    echo "<h1>Hola, $nombre</h1>";
+    print("Tu edad es: $edad años<br>");
+
+    ?>
+</body>
+</html>
