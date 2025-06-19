bfda7eb0e16c4705c9b540e342b42d2bbc5605ce
JdaniloChin <danilochin91@gmail.com>
2025-06-18 20:05:07 -0600

    commit 3

--- clase06.php
+++ clase06.php
@@ -12,20 +12,47 @@
     // Manejo de variables 
     $nombre= "Carlos";
     $edad = 30;
-    $nota = 85.5;
+    $nota1 = 85.5;
+    $nota2 = 60.4;
+    $nota3 = 70.8;
+    $nota4 = 40.3;
+
     $aprobado= true;
 
     // Imprimir 
+    echo "<header>";
     echo "<h1>Hola, $nombre</h1>";
     print("Tu edad es: $edad años<br>");
-    echo "<p>";
+    echo "</header>";
+    echo "<section><p>";
     // Condicionales
     if($edad >= 18){
         echo "Eres mayor de edad.";
     } else {
         echo "Eres menor de edad."; 
     }
-    echo "</p>";
+    echo "</p></section>";
+
+    // Operadores aritméticos
+    $suma =0;
+    $arr_notas = array($nota1, $nota2, $nota3, $nota4);
+    for($i =0; $i < count($arr_notas); $i++){
+        $suma += $arr_notas[$i];
+    }
+    $promedio = 0;
+    $promedio = $suma / count($arr_notas);
+
+    if($promedio < 70){
+        $aprobado = false;
+    }
+    echo "<section>";
+    echo "<p>Tu promedio es: $promedio</p>";    
+    if($aprobado){
+        echo "<p>¡Felicidades! Has aprobado.</p>";
+    } else {
+        echo "<p>Lo siento, no has aprobado.</p>";
+    }
+    echo "</section>";
     ?>
 </body>
 </html>
