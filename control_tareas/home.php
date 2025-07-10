23a3134fe350b91964b1ea7540ba654d23944bae
JdaniloChin <danilochin91@gmail.com>
2025-07-09 18:29:36 -0600

    agregar menu

--- control_tareas/home.php
+++ control_tareas/home.php
@@ -13,8 +13,23 @@ session_start();
     <title>Inicio</title>
 </head>
 <body>
+  <div class="container-fluid">
+    <div class="row min-vh-100">
+  <aside class="col-md-3 bg-dark text-white p-4">
+        <h4 class="mb-4">Menú</h4>
+        <ul class="nav flex-column">
+          <li class="nav-item"><a class="nav-link text-white" href="#">Inicio</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="#">Usuarios</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="#">Reportes</a></li>
+          <li class="nav-item"><a class="nav-link text-white" href="#">Configuración</a></li>
+        </ul>
+      </aside>
+      <main class="col-md-9 p-4">
     <?php
       echo "<H1 clasa='text-center'>Bienvenido - " . $_SESSION['nombre'] . "</H1>"; 
     ?>
+    </main>
+    </div>
+  </div>
 </body>
 </html>
